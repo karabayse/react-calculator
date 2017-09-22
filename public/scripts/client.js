0f6f48cdff34d7efa0635e4376ad26a970b07db0
Kara Bayse <karabayse@gmail.com>
2017-09-21 19:44:48 -0500

    added React.createElement and ReactDOM.render code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,27 @@
 console.log('in client.js');
 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// class MyComponentClass extends React.Component {
+//   render() {
+//     return <h1>Hello world</h1>;
+//   }
+// };
+
+const title = React.createElement(
+  'h3',
+  {id:'title', className: 'header'},
+  'enter calculation data here'
+);
+
+ReactDOM.render(
+  title, // first argment
+  document.getElementById('react-container'),
+)
+
+// ReactDOM.render(
+//   <MyComponentClass />,
+//   document.getElementById('server') // app
+// );
 // ReactDOM.render(<h1>Hello world</h1>, document.getElementById('server'));
