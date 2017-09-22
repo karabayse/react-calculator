33a40941efda9f007469d0fc1d43da1dc8e7f752
Kara Bayse <karabayse@gmail.com>
2017-09-22 14:47:47 -0500

    npm installed babel

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,26 +2,14 @@ console.log('in client.js');
 
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-//
-// class MyComponentClass extends React.Component {
-//   render() {
-//     return <h1>Hello world</h1>;
-//   }
-// };
 
-const title = React.createElement(
+const instructions = React.createElement(
   'h3',
-  {id:'title', className: 'header'},
+  {id:'instructions', className: 'header'},
   'enter calculation data here'
 );
 
 ReactDOM.render(
-  title, // first argment
+  instructions, // first argment
   document.getElementById('react-container'),
-)
-
-// ReactDOM.render(
-//   <MyComponentClass />,
-//   document.getElementById('server') // app
-// );
-// ReactDOM.render(<h1>Hello world</h1>, document.getElementById('server'));
+);
